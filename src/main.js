0fec89d22ced9d74c41d10c361227e08339f8dab
jase-d-ace <andrada.jasonp@gmail.com>
2020-10-03 12:52:58 -0400

    instantiate and inject store into App

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store/'
 
 const app = createApp(App);
 
 app.config.productionTip = false
 
+app.use(store)
 app.mount("#app")
 
