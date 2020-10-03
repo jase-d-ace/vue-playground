bd10758cfe0b2b364e6a4a00578badd398ba793b
jase-d-ace <andrada.jasonp@gmail.com>
2020-10-03 16:54:12 -0400

    create basic input component with a v-model

---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'Input',
+    data() {
+      return {
+        message: "Learning me a Vue things"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1> {{ message }}</h1>
+  <input type="text" v-model="message" />
+</template>
