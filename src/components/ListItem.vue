84a7179c947f217857dd078dc923683eb284b5cd
jase-d-ace <andrada.jasonp@gmail.com>
2020-10-06 13:10:20 -0400

    create ListItem to create a new component for every element in state array

---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    guitar: Object
+  }
+}
+</script>
+
+<template>
+  <div class="list-item">
+    <li class="some-class">
+      <p>{{ guitar.brand }} {{ guitar.model }}</p>
+    </li>
+  </div>
+</template>
