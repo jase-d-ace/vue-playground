5145340a4619108bac2caad540a4ed4154b2297c
jase-d-ace <andrada.jasonp@gmail.com>
2020-10-06 13:09:52 -0400

    create ListContainer and add logic for iterative rendering

---
+++ src/components/ListContainer.vue
@@ -0,0 +1,21 @@
+<script>
+import ListItem from './ListItem.vue'
+export default {
+  name: 'ListContainer',
+  props: {
+    guitars: Array
+  },
+  components: {
+    ListItem
+  }
+}
+</script>
+
+<template>
+  <div class="list">
+    <h1>Guitars</h1>
+    <ul v-bind:key="guitar" v-for="guitar in guitars" class="vue-list">
+      <ListItem :guitar="guitar" />
+    </ul>
+  </div>
+</template>
